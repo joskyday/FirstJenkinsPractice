6f2b4e472b4f4965167d2511c6f352669c08a189
joskyday <joskyday@gmail.com>
2022-04-27 15:36:47 +0300

    new stuff second time

--- src/test/java/MyFirstJenkinsApp.java
+++ src/test/java/MyFirstJenkinsApp.java
@@ -2,5 +2,6 @@ public class MyFirstJenkinsApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //this is added
+        //this is added again
     }
 }
