21fe4ede1e5231a339ae35dce7a56679327d885e
joskyday <joskyday@gmail.com>
2022-04-30 05:25:17 +0300

    cropped

--- src/test/java/MyFirstJenkinsApp.java
+++ src/test/java/MyFirstJenkinsApp.java
@@ -1,11 +1,6 @@
 public class MyFirstJenkinsApp {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        //this is added
-        //this is added again
-        //again
-        //send email report
-        System.out.println("Hello again! ");
     }
 
 
